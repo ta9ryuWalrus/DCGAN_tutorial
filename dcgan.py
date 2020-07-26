2fb9d28bfda991b8d3fa768a8fd4225d643ed8db
ta9ryuWalrus <al5.b00taku@gmail.com>
2020-07-26 12:39:16 +0900

    dcgan

--- dcgan.py
+++ dcgan.py
@@ -21,26 +21,36 @@ torch.manual_seed(manualSeed)
 
 # Root directory for dataset
 dataroot = "data/celeba"
-
 # Number of workers for dataloader
 workers = 8
-
 batch_size = 128
 image_size = 64
-
 # Number of channels in the training images.
 nc = 3
-
 # Size of z latent vector
 nz = 100
-
 # Size of feature maps in generator
 ngf = 64
-
 # Size of feature maps in discreminator
 ndf = 64
-
 num_epochs = 5
 lr = 0.0002
 beta1 = 0.5
 ngpu = 1
+
+dataset = dset.ImageFolder(root=dataroot,
+                           transrom=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+
+dataloader = torch.utils.data.Dataloader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
