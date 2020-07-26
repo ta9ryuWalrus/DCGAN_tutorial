2db14bcd661dee0b233eadaa3b23c1926dc169a1
ta9ryuWalrus <al5.b00taku@gmail.com>
2020-07-26 11:55:52 +0900

    gitignore

---
+++ dcgan.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+manualSeed = random.randint(1,10000)
+print("Random Seed: ", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
+
+# Root directory for dataset
+dataroot = "data/celeba"
+
+# Number of workers for dataloader
+workers = 8
+
+batch_size = 128
+image_size = 64
+
+# Number of channels in the training images.
+nc = 3
+
+# Size of z latent vector
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discreminator
+ndf = 64
+
+num_epochs = 5
+lr = 0.0002
+beta1 = 0.5
+ngpu = 1
